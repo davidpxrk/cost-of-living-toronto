10237de0b31aa2a1cf7d441b5a60c3665eb6ed3e
davidpxrk <hyunje.park@mail.utoronto.ca>
2024-09-21 11:55:31 -0400

    fixed

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,43 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from OpenDataToronto
+# Author: Hyunje Park
+# Date: 24 September 2024
+# Contact: hyunje.park@mail.utoronto.ca 
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
 
 #### Workspace setup ####
+#install.packages('opendatatoronto')
+#install.packages("dplyr")
+#install.packages('tidyverse')
+#install.packages('sf')
+
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
-
+library(dplyr)
+library(sf)
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 
+# get package
+link <- "52182614-1f0b-42be-aca4-3f86dc8e004c"
 
+package <- show_package(link)
+package
+
+# get all resources for this package
+resources <- list_package_resources(link)
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+datastore_resources
+
+# load the first data store resource as a sample
+raw_data <- filter(resources,row_number()==1) %>% get_resource()
+raw_data
 
 #### Save data ####
-# [...UPDATE THIS...]
+
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(raw_data, "data/raw_data/raw_data.csv") 
 
          
